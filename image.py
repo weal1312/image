0e29c184de82b977d5620cdab02f0286750e2fb9
weal1312 <1071993+weal1312@users.noreply.github.com>
2021-02-01 19:35:40 +0800

    merge main.py into image.py

--- image.py
+++ image.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from PIL import Image
 
@@ -24,3 +25,19 @@ class IMG:
 
 		img_l.save(os.path.join(self.dest_dir, self.filename + '_2.png'))
 		img_r.save(os.path.join(self.dest_dir, self.filename + '_1.png'))
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('mod', help='define the type image process(crop|split)')
+	parser.add_argument('-i', '--input', help='define the input path')
+	parser.add_argument('-o', '--output', help='define the output path')
+	args = parser.parse_args()
+
+	method = args.mod
+	source_dir = args.input if args.input else 'E:\\Downloads\\temp'
+	dest_dir = args.output if args.output  else os.path.join(source_dir, 'output')
+	os.makedirs(dest_dir, exist_ok=True)
+
+	for path in [os.path.join(source_dir, file) for file in os.listdir(source_dir) if file.endswith('png')]:
+		getattr(IMG(path, dest_dir), method)()
